e633f7d09ade30e564c4c261a592e829388c01ac
Dinitha Fernando <Dinithatharuka@gmail.com>
2024-08-06 16:26:30 +0530

    Delete FlyMaster directory

--- FlyMaster/import/heading.php
+++
@@ -1,8 +0,0 @@
-<h1 class="maint">Where Dreams Take Flight</h1><br>
-<h1 class="welcome"><?php 
-                    if(isset($_SESSION["fname"])){
-                    echo 'Welcome ' . $_SESSION["fname"].' !';
-                    } else {
-                        echo 'Hello New User !';
-                    }
-                    ?></h1>
